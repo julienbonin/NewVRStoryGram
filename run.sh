9f88a0f99b0060b9e43fd198043a3d8cbe2122ec
tfournet <tfournet@radersolutions.com>
2018-12-03 14:17:19 -0600

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,11 @@
+#!/bin/sh
 
+isvr_version="1.1.5"
+echo "installing" \
+    && cd /tmp/ \
+    && wget https://github.com/IdeaSpaceVR/IdeaSpace/releases/download/v${isvr_version}/IdeaSpace-${isvr_version}.tar.gz \
+    && tar zxvf IdeaSpace-${isvr_version}.tar.gz \
+    && mv IdeaSpace${isvr_version}/* /var/www/html/ \
+    && chown -R apache.apache /var/www/html/*
+    
+    
