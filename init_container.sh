cd40d7bbdf2929ff65a0b7be133af84695539cc7
tfournet <tfournet@radersolutions.com>
2018-12-04 10:38:20 -0600

    Update init_container.sh

--- init_container.sh
+++ init_container.sh
@@ -16,13 +16,3 @@ cat /etc/motd
 
 service ssh start
 
-mkdir "$PM2HOME"
-chmod 777 "$PM2HOME"
-ln -s /home/LogFiles "$PM2HOME"/logs
-
-echo "$@" > /opt/startup/startupCommand
-node /opt/startup/generateStartupCommand.js
-
-STARTUPCOMMAND=$(cat /opt/startup/startupCommand)
-echo "Running $STARTUPCOMMAND"
-eval "exec $STARTUPCOMMAND"
