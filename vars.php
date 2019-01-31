cab9188d8de49363757d881f01d74cf6194de4d4
tfournet <tfournet@radersolutions.com>
2019-01-31 13:58:35 -0600

    Update vars.php

--- vars.php
+++ vars.php
@@ -3,5 +3,7 @@ var_dump(getenv('mysql_host'));
 var_dump(getenv('mysql_database'));
 var_dump(getenv('mysql_userid'));
 var_dump(getenv('mysql_password'));
+var_dump(getenv('app_debug'));
+var_dump(getenv('mysql_prefix'));
 var_dump(getenv('defaultConnection'));
 ?>
