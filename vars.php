da3024aecc8c7dd6d2515188476962a98c1dda92
tfournet <tfournet@radersolutions.com>
2019-01-31 12:36:05 -0600

    Update vars.php

--- vars.php
+++ vars.php
@@ -1,6 +1,7 @@
 <?php
 var_dump(getenv('mysql_host'));
+var_dump(getenv('mysql_database'));
+var_dump(getenv('mysql_userid'));
+var_dump(getenv('mysql_password'));
 var_dump(getenv('defaultConnection'));
-env(radertestslot);
-env('mysql_host');
 ?>
