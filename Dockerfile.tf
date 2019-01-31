53980809034991de11c8653ab303dc2b2c0062c1
tfournet <tfournet@radersolutions.com>
2019-01-31 13:13:40 -0600

    Update Dockerfile.tf

--- Dockerfile.tf
+++ Dockerfile.tf
@@ -32,3 +32,5 @@ RUN docker-php-ext-install pdo_mysql
 RUN ln -s ../mods-available/{expires,headers,rewrite}.load /etc/apache2/mods-enabled/
 RUN sed -e '/<Directory \/var\/www\/>/,/<\/Directory>/s/AllowOverride None/AllowOverride All/' -i /etc/apache2/apache2.conf
 RUN apachectl -k graceful
+
+EXPOSE 80 2222
