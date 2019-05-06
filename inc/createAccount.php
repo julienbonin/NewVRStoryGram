1794128766a3665679339ab59796c0d609b24bd1
Julien <julienbonin@users.noreply.github.com>
2019-05-05 21:54:38 -0500

    Site Files Upload
    Uploading /img, /inc, /img, and /css directories to IdeaSpace root

---
+++ inc/createAccount.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link rel="stylesheet" href="/css/normalize.css">
+  <link rel="stylesheet" href="/css/style.css">
+</head>
+
+<body>
+  <header>
+    <div class="logo_div"><a href="/index.php"><img src="/img/LOGO.jpg" alt="VRStoryGram Logo" id="logo"></a></div>
+    <div class="nav_1_div"><a href="readme.php">What is VRStoryGram</a></div>
+    <div class="nav_2_div"><a href="createAccount.php">Create</a></div>
+    <div class="nav_3_div"><a href="about.php">Team VRStroyGram</a></div>
+    <div class="nav_4_div"><a href="login.php">Login</a></div>
+  </header>
+  <div class="content_div">
+    <div class="createAccount_content_div">
+
+    <div class="new_account_div">
+      <h1>Create VRStoryGram Account</h1>
+      <form>
+        First Name<input type="text" name="first name"><br>
+        Last Name<input type="text" name="last name"><br>
+        Email<input type="text" name="email"><br>
+        City<input type="text" name="city"><br>
+        State<input type="text" name="state"><br>
+      </form>
+
+
+    </div>
+
+    <div class="go_to_login_div">
+
+      <a href="login.php"><h1>Go To Login</h1></a>
+
+    </div>
+
+    </div>
+
+
+  </div>
+  <footer>
+    <div class="social_media_div">
+      <h2>Get Social!</h2>
+      <div class="social_media_icon_div">
+        <a href="#" class="fa fa-facebook"></a>
+        <a href="#" class="fa fa-youtube"></a>
+        <a href="#" class="fa fa-instagram"></a>
+        <a href="#" class="fa fa-twitter"></a>
+      </div>
+    </div>
+    <div class="secondary_nav_div"></div>
+  </footer>
+</body>
+</html>
